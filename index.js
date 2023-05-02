f8c83f8219ca12308933d9aae19107cb472877a3
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-05-02 19:35:12 +0600

    Individual Chef API Created

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors());
 
 
 const allChef = require('./data/chef-details.json');
@@ -13,6 +15,14 @@ app.get('/', (req, res) => {
 
 app.get('/all-chef', (req, res) => {
     res.send(allChef)
+});
+
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(id);
+
+    const chef = allChef.find(c => c.id == id)
+        res.send(chef)
 });
 
 
